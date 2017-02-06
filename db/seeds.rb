5c4f200ca1b3bb266d7e61059b5416a711ec47a8
ReidasaurusRex <reidhlewis91@gmail.com>
2017-02-06 15:38:48 -0800

    Added a dozen haikus in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Seeding with a couple of haikus
+Haiku.create([
+  {line1: 'Blowing from the West', line2: 'Fallen leaves gather', line3: 'In the East.'},
+  {line1: 'An old silent pond...', line2: 'A frog jumps into the pond,', line3: 'splash! Silence again.'},
+  {line1: 'First autumn morning', line2: 'the mirror I stare into', line3: 'shows my father\'s face.'},
+  {line1: 'A summer river being crossed', line2: 'how pleasing', line3: 'with sandals in my hands!'},
+  {line1: 'Ground squirrel', line2: 'balancing its tomato', line3: 'on the garden fence.'},
+  {line1: 'A cricket disturbed', line2: 'the sleeping child; on the porch', line3: 'a man smoked and smiled.'},
+  {line1: 'Sharp peaks jab the sky', line2: 'dark pines wear thick coats of snow', line3: 'blue skies over all'},
+  {line1: 'Sierra Rockies', line2: 'young bucks compared to', line3: 'old granny Catskills.'},
+  {line1: 'Door blocked by drifts, wind', line2: 'knocks me back. Cold smack of snow', line3: 'shouts Stay Inside Fool'},
+  {line1: 'This is why she can\'t', line2: 'move back East, the cubicle', line3: 'life too dim, dull, stale.'},
+  {line1: 'We all crave power,', line2: 'connection. Squat shamelessly', line3: 'for that next tech fix.'},
+  {line1: 'Buying a domain', line2: 'name to monetize content.', line3: 'The new bourgeoisie.'},
+  {line1: 'Two sides divided', line2: 'Looming threats go unaddressed', line3: 'I\'ll do what I can'},
+  {line1: 'Lightyears between us', line2: 'Missing your words and your laughs', line3: 'For now: a phone call'}
+])
