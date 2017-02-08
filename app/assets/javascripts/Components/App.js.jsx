e40b719098d288fcfdec877add11957b55e7afd8
ReidasaurusRex <reidhlewis91@gmail.com>
2017-02-07 16:25:27 -0800

    Added nav bar with basic styling. Still need to add access functionality

--- app/assets/javascripts/Components/App.js.jsx
+++ app/assets/javascripts/Components/App.js.jsx
@@ -2,8 +2,10 @@ class App extends React.Component {
   constructor() {
     super();
     this.properPage = this.properPage.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    this.handleCreateClick = this.handleCreateClick.bind(this);
+    this.handleTitleClick = this.handleTitleClick.bind(this);
     this.handlePost = this.handlePost.bind(this);
+    this.handleAccessClick = this.handleAccessClick.bind(this);
     this.state = {
       currentPage: 'index'
     }
@@ -15,8 +17,14 @@ class App extends React.Component {
       return (<CreateHaiku url={this.props.url} successfulPost={this.handlePost}/>);
     }
   }
-  handleClick() {
+  handleAccessClick() {
+    
+  }
+  handleCreateClick() {
     this.setState({currentPage: 'create'});
+  }
+  handleTitleClick() {
+    this.setState({currentPage: 'index'})
   }
   handlePost() {
     this.setState({currentPage: 'index'});
@@ -24,8 +32,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <button onClick={this.handleClick}>Create</button>
-        <h1>Haiku Haven</h1>
+        <NavBar sendTitleClick={this.handleTitleClick} sendCreateClick={this.handleCreateClick} sendAccessClick={this.handleAccessClick}/>
         {this.properPage()}
       </div>
     );
--- app/assets/javascripts/Components/NavBar.js.jsx
+++ app/assets/javascripts/Components/NavBar.js.jsx
@@ -1,11 +1,25 @@
 class NavBar extends React.Component {
   constructor() {
     super();
+    this.signInOrOut = this.signInOrOut.bind(this);
+  }
+  signInOrOut() {
+    if (this.props.signedIn) {
+      return <li className="c-nav__list__li" onClick={this.props.sendSignOut}>Sign Out</li>;
+    } else {
+      return <li className="c-nav__list__li" onClick={this.props.sendSignIn}>Sign In</li>;
+    }
   }
   render() {
     return (
-      <nav>
-        <h2>Haiku Haven</h2>
+      <nav className="c-nav">
+        <h3 className="c-nav__title" onClick={this.props.sendTitleClick}>
+          Haiku Haven
+        </h3>
+        <ul className="c-nav__list">
+          <li className="c-nav__list__li" onClick={this.props.sendCreateClick}>Create</li>
+          {this.signInOrOut()}
+        </ul>
       </nav>
     );
   }
